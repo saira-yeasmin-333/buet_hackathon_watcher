f7c5acc5a5d1b4c39154fa017d3c488dcff61f2d
saira-yeasmin-333 <sairayeasminsa@gmail.com>
2022-08-04 10:41:51 +0600

    first commit

---
+++ app/src/main/java/com/example/buet_hackathon_22/textToSpeech/TextToSpeechActivity.java
@@ -0,0 +1,16 @@
+package com.example.buet_hackathon_22.textToSpeech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.buet_hackathon_22.R;
+
+public class TextToSpeechActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_to_speech);
+    }
+}
